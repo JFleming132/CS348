6c055d8cec72ee1c82acd3f2a6ec1f8d74d2892a
JFleming132 <josephflemingvi@gmail.com>
2024-04-22 19:09:25 -0400

    beginningto implement aggregation on the report

--- server/backend/index.js
+++ server/backend/index.js
@@ -151,6 +151,57 @@ app.post("/api/trips/delete",(req,res)=>{
 })
 
 
+app.post("/api/trips/filter", (req, res)=>{
+    console.log("recieved form from React for filter trips");
+    console.log("body is " + JSON.stringify(req.body));
+    let q = "SELECT trip.*, " + 
+            //"COUNT(t2.id) AS overlapCount, " +
+            "TIMEDIFF(trip.endTime, trip.startTime) as duration, " + 
+            "make, " +
+            "model, " + 
+            "firstName, " + 
+            "lastName, " +
+            "startWarehouse.id as startWarehouseID, " +
+            "endWarehouse.id as endWarehouseID, " + 
+            "endWarehouse.name as destinationName, " + 
+            "endWarehouse.address as destinationAddress, " + 
+            "startWarehouse.name as startName, " + 
+            "startWarehouse.address as startAddress " +
+            "FROM trip " +
+            /*"JOIN trip AS t2 ON trips.id != t2.id AND " +
+            "  ((trips.startTime BETWEEN t2.startTime AND t2.endTime) OR " + 
+            "  (trips.endTime BETWEEN t2.startTime AND t2.endTime)) " + */
+            "JOIN driver ON driver.licenseNumber = trip.driverLicenseNumber " +
+            "JOIN truck ON truck.plateNumber = trip.truckPlateNumber " +
+            "JOIN warehouse startWarehouse ON startWarehouse.id = startWarehouseID " +
+            "JOIN warehouse endWarehouse ON endWarehouse.id = endWarehouseID " + 
+            "WHERE ";
+    if ((req.body.minID != undefined) && (req.body.minID != "")) {
+        q = q + "trip.id >= " + req.body.minID + " AND ";
+    }
+    if ((req.body.maxID != undefined) && (req.body.maxID != "")) {
+        q = q + "trip.id <= " + req.body.maxID + " AND ";
+    }
+    if ((req.body.fromWarehouseID != undefined) && (req.body.fromWarehouseID != "")) {
+        q = q + "startWarehouseID = " + req.body.fromWarehouseID + " AND "
+    }
+    if ((req.body.toWarehouseID != undefined) && (req.body.fromWarehouseID != "")) {
+        q = q + "endWarehouseID = " + req.body.toWarehouseID + " AND "
+    }
+    if ((req.body.driverID != undefined) && (req.body.driverID != "")) {
+        q = q + "driver.licenseNumber = " + req.body.driverID + " AND "
+    }
+    q = q + "1 = 1 ORDER BY trip.id";
+    console.log(q)
+    db.query(q, (err,data) => {
+        if (err) {
+            console.log(err)
+            return res.json(err)
+        }
+        res.json(data)
+    })
+})
+
 app.listen(8800,()=>{
     console.log("Backend API listening on port 8800")
 })
--- server/client/src/components/TripsFilter.js
+++ server/client/src/components/TripsFilter.js
@@ -1,18 +1,19 @@
 import {useState, useEffect} from 'react'
 
 const TripsFilter = () => {
-    const [minID, updateMinID] = useState({});
-    const [maxID, updateMaxID] = useState({});
-    const [toWarehouseID, updateToWarehouseID] = useState({});
-    const [fromWarehouseID, updateFromWarehouseID] = useState({});
-    const [driverID, updateDriverID] = useState({});
-    const [truckID, updateTruckID] = useState({});
-    const [startDateMin, updateStartDateMin] = useState({});
-    const [startDateMax, updateStartDateMax] = useState({});
-    const [endDateMin, updateEndDateMin] = useState({});
-    const [endDateMax, updateEndDateMax] = useState({});
-    const [durationMin, updateDurationMin] = useState({});
-    const [durationMax, updateDurationMax] = useState({});
+    const [minID, updateMinID] = useState(undefined);
+    const [maxID, updateMaxID] = useState(undefined);
+    const [toWarehouseID, updateToWarehouseID] = useState(undefined);
+    const [fromWarehouseID, updateFromWarehouseID] = useState(undefined);
+    const [driverID, updateDriverID] = useState(undefined);
+    const [truckID, updateTruckID] = useState(undefined);
+    const [startDateMin, updateStartDateMin] = useState(undefined);
+    const [startDateMax, updateStartDateMax] = useState(undefined);
+    const [endDateMin, updateEndDateMin] = useState(undefined);
+    const [endDateMax, updateEndDateMax] = useState(undefined);
+    const [durationMin, updateDurationMin] = useState(undefined);
+    const [durationMax, updateDurationMax] = useState(undefined);
+    const [trips, setTrips] = useState([]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -31,6 +32,21 @@ const TripsFilter = () => {
             durationMax:durationMax,
         }
         console.log(filterOptions);
+        const requestOptions = {
+            method: "POST",
+            headers: {
+            "Content-Type":"application/json"
+            },
+            body: JSON.stringify(filterOptions)
+        }
+        fetch("/api/trips/filter", requestOptions).then(response=>response.json()).then(data=> {
+            if (data.errno) {
+                alert("SQL error. Errno: " + data.errno)
+            } else {
+                setTrips(data);
+            }
+        })
+
         //send the filters to the backend and use a stored procedure to return a query
     }
     //set up forms for the rest of the fields
@@ -42,8 +58,45 @@ const TripsFilter = () => {
                 <input name="minIDInput" type="number" onChange={e=>{updateMinID(parseInt(e.target.value))}}/>
                 Maximum Trip ID
                 <input name="maxIDInput" type="number" onChange={e=>{updateMaxID(parseInt(e.target.value))}}/>
+                Start Warehouse ID
+                <input name="startWarehouseID" type="number" onChange={e=>{updateFromWarehouseID(parseInt(e.target.value))}}/>
+                Destination Warehouse ID
+                <input name="endWarehouseID" type="number" onChange={e=>{updateToWarehouseID(parseInt(e.target.value))}}/>
+                Driver License Number
+                <input name="driverID" type="text" onChange={e=>{updateDriverID(e.target.value)}}/>
+                Truck Plate Number
+                <input name="truckID" type="text" onChange={e=>{updateTruckID(e.target.value)}}/>
                 <button>Search</button>
             </form>
+            <table>
+                <tr>
+                    <th>Trip ID</th>
+                    <th>Driver Name</th>
+                    <th>Truck Make and Model</th>
+                    <th>Departure Warehouse Name and Address</th>
+                    <th>Destination Warehouse Name and Address</th>
+                    <th>Departure Time</th>
+                    <th>Arrival Time</th>
+                    <th>Trip Duration</th>
+                    <th>Overlapping Trips</th>
+                </tr>
+                {trips.map(trip => {
+                //console.log(trip);
+                    return (
+                        <tr>
+                            <td>{trip.id}</td>
+                            <td>{trip.firstName} {trip.lastName}</td>
+                            <td>{trip.make} {trip.model}</td>
+                            <td>{trip.startName} at address {trip.startAddress}</td>
+                            <td>{trip.destinationName} at address {trip.destinationAddress}</td>
+                            <td>{trip.startTime}</td>
+                            <td>{trip.endTime}</td>
+                            <td>{trip.duration}</td>
+                            
+                        </tr>
+                    )
+            })}
+            </table>
         </div>
     )
 }
